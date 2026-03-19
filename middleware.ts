628532fadaeb5788d774bbdcc2d404ffcd246b22
jrsana22 <jrsana@yahoo.com.br>
2026-03-19 00:27:20 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -23,7 +23,10 @@ export async function middleware(request: NextRequest) {
     }
   )
 
-  const { data: { user } } = await supabase.auth.getUser()
+ // DEPOIS
+const { data: { session } } = await supabase.auth.getSession()
+const user = session?.user ?? null
+
 
   const isAuthRoute = request.nextUrl.pathname.startsWith('/login')
   const isApiRoute = request.nextUrl.pathname.startsWith('/api')
